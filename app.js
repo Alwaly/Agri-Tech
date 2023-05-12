b67d2ad40968109f8a791c0ae1e015f9ad0c193e
AlwalyLengo <alwaly@lengo.africa>
2023-05-12 18:26:08 +0000

    Seted form

---
+++ app.js
@@ -0,0 +1,37 @@
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the form from submitting and refreshing the page
+    console.log("hello")
+    // Get form values
+    var n = document.getElementById("n").value;
+    var p = document.getElementById("p").value;
+    var k = document.getElementById("k").value;
+    var temperature = document.getElementById("temperature").value;
+    var humidity = document.getElementById("humidity").value;
+    var ph = document.getElementById("ph").value;
+    var rainfall = document.getElementById("rainfall").value;
+    
+    // Create data object with form values
+    var data = {
+        n: n,
+        p: p,
+        k: k,
+        temperature: temperature,
+        humidity: humidity,
+        ph: ph,
+        rainfall: rainfall
+    };
+    console.log(JSON.stringify(data));
+    // Make the POST request
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://127.0.0.1:8000/predict", true);
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // Request successful, do something with the response
+        console.log(xhr.responseText);
+      }
+    };
+    
+    xhr.send(JSON.stringify(data));
+  });
+  
